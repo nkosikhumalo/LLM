3062a28aff1e2d83b7044769b157eba7e962f6f5
nkosikhumalo <Nkosi_10@outlook.com>
2026-09-24 10:33:00 +0200

    test(java): add package info for test suite

---
+++ java/src/test/java/com/microllm/PackageInfo.java
@@ -0,0 +1,9 @@
+package com.microllm;
+
+/**
+ * Belongs here: unit tests for tensor ops, softmax, gradient checks, layer shapes, export round-trips.
+ *
+ * Add concrete *Test.java classes alongside implementation work.
+ */
+class PackageInfo {
+}
